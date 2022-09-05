69391ad98b94ea3cc0cc020f054aaa97a42f286a
TimurBatyr <it.timurbatyr@gmail.com>
2022-09-05 16:16:10 +0600

    chat in progress

--- chat/urls.py
+++ chat/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import ChatAPIView
+from .views import ChatAPIView, message_list, user_list
 
 urlpatterns = [
 
     path("chat/", ChatAPIView.as_view()),
+    path('messages/<int:sender>/<int:receiver>/', message_list),
+    path('messages/', message_list),
+    path('users/', user_list),
 
 ]
