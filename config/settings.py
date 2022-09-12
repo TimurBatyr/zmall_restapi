9a4f8494cd90a42ffcf89f0828c491ebb8f9092b
TimurBatyr <it.timurbatyr@gmail.com>
2022-09-12 16:50:41 +0600

    fixed docker

--- config/settings.py
+++ config/settings.py
@@ -157,7 +157,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10000
+    'PAGE_SIZE': 1000
 }
 
 SIMPLE_JWT = {
