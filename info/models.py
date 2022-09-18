30d07378d1562047a0e298e62dc417028709e0d7
TimurBatyr <it.timurbatyr@gmail.com>
2022-09-18 17:06:57 +0600

    draft help

--- info/models.py
+++ info/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Issue(models.Model):
+    """Category for post"""
+    title = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Help(models.Model):
+    issue = models.ForeignKey(Issue, related_name='help', on_delete=models.CASCADE)
+    description = models.TextField()
+
+
